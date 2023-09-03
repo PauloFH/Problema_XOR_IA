194a85bbf23512c20dd6fbeecaba8b7adae449d6
Paulo Holanda <paulorfholanda@gmail.com>
2023-09-03 15:21:27 -0300

    Correção dos passos

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Problema_XOR_IA
 
 Desenvolvimento de uma Rede neural que aprenda a identificar uma entrada binária de dois bits e retorne de acordo com o XOR:
+
  se 0,0 -> 0
  se 1,1 -> 0
  se 1,0 -> 1
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Função de ativação sigmoide
+# Função sigmoide
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
@@ -8,7 +8,7 @@ def sigmoid(x):
 def sigmoid_derivada(x):
     return x * (1 - x)
 
-# Função degrau
+# Função degrau que irá transformar a saida em 0 ou 1
 def funcao_degrau(x):
     return 1 if x >= 0.5 else 0
 
@@ -19,16 +19,17 @@ tamanho_oculta = 2
 tamanho_saida = 1
 taxa_aprendizado = 0.1
 epocas = 10000
-
-# Passo 0
 np.random.seed(0)
-pesos_entrada_oculta = np.random.uniform(size=(tamanho_entrada, tamanho_oculta))
-pesos_oculta_saida = np.random.uniform(size=(tamanho_oculta, tamanho_saida))
 
 
-# Passo 1
+
+
 entradas = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 saidas_esperadas = np.array([[0], [1], [1], [0]])
+
+
+pesos_entrada_oculta = np.random.uniform(size=(tamanho_entrada, tamanho_oculta))
+pesos_oculta_saida = np.random.uniform(size=(tamanho_oculta, tamanho_saida))
 
 # Treinamento da rede neural
 for epoca in range(epocas):
@@ -42,28 +43,32 @@ for epoca in range(epocas):
         # Passo 3
         saida_oculta = sigmoid(entrada_oculta)
         
-        # Passo 3
+        # Passo 4
         entrada_saida = np.dot(saida_oculta, pesos_oculta_saida)
         
-        # Passo 4
-        saida_prevista = sigmoid(entrada_saida)
-    
         # Passo 5
-        erro = saidas_esperadas[i] - saida_prevista
-        erro_total += np.sum(erro ** 2)
-    
-    
+        saida_prevista = sigmoid(entrada_saida)
+
         # Passo 6
+        erro = saidas_esperadas[i] - saida_prevista
         d_saida = erro * sigmoid_derivada(saida_prevista)
         
         # Passo 7
         d_oculta = d_saida.dot(pesos_oculta_saida.T) * sigmoid_derivada(saida_oculta)
     
-        # Atualização dos pesos
+        # Passo 8
         pesos_oculta_saida += np.outer(saida_oculta, d_saida) * taxa_aprendizado
+        
+        # Passo 9
         pesos_entrada_oculta += np.outer(entradas[i], d_oculta) * taxa_aprendizado
+        
+        # Passo 10
+        erro_total += np.sum(erro ** 2)
+        
     if epoca % 1000 == 0:
         print(f"Época {epoca}, Erro Médio: {erro_total / len(entradas)}")
+        
+
 temp = True
 array = [0, 0]
 
